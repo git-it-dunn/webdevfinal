d2d7c5a3e40acfd8cf01ac93bc02cbb4c0d52522
Zachariah Friesen <zachariah.friesen@ontariotechu.net>
2023-04-18 17:33:41 -0400

    added functionality to wordspage

--- scripts/wordpull.js
+++ scripts/wordpull.js
@@ -1,5 +1,7 @@
 function dictionarypull(a){
     $.get("https://wordsapiv1.p.mashape.com/words/"+a+"/definitions", function(b){
-        document.getElementById('dict').innerHTML = "<br> <p>Definitions: "+b+"</p> <br>";
+        var temp = JSON.stringify(b);
+        var data = JSON.parse(temp);
+        document.getElementById('dict').innerHTML = "<br> <p>Definitions: "+data.Search[0].definition+"</p> <br>";
     });
 }
