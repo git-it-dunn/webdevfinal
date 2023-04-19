5ffcac9bfc0ef49fc8156bc5619844c2c905db8a
J6k3School <jacob.turner2@ontariotechu.net>
2023-04-19 02:42:43 -0400

    better working home page

--- scripts/maingamepage.js
+++ scripts/maingamepage.js
@@ -1,50 +1,62 @@
+let word = "WORD";
+var lSel = null;
+
+let keys = "Q,W,E,R,T,Y,U,I,O,P,A,S,D,F,G,H,J,K,L,Z,X,C,V,B,N,M,Enter".split(',');
+let numFail = 0;
+
 window.onload = function(){
     makingWordlboxes();
     makingKeyboard();
 }
 
+
 function makingKeyboard(){
-    let keys = "Q,W,E,R,T,Y,U,I,O,P,A,S,D,F,G,H,J,K,L,Z,X,C,V,B,N,M".split(',');
-    let input = "Backspace,Enter".split(',');
-
-    let temp = 0;
-    for (let r = 0; r < 13; r++) {
-        for (let c = 0; c < 2; c++) {
-            let bord = document.createElement("div");
-            bord.id = r.toString() + "-" + c.toString();
-            bord.innerText = keys[temp];
-            bord.classList.add("letter");
-            document.getElementById("keyboard").append(bord);
-            temp++;
-        }
+    //This creates the makeshift keyboard at the bottom of the game and gives all the clicking on the letters abilitys to work
+    for (let a = 0; a < keys.length; a++) {
+        let bord = document.createElement("div");
+        bord.id = a;
+        bord.innerText = keys[a];
+        bord.addEventListener("click", letterSel);
+        bord.classList.add("letter");
+        document.getElementById("keyboard").append(bord);
     }
-
-    for (let i = 0; i < input.length; i++){
-        let inputButton = document.createElement("div");
-        inputButton.id = i;
-        inputButton.innerText = input[i];
-        inputButton.classList.add("inputButtonLetters");
-        document.getElementById("keyboard").appendChild(inputButton);
-    }
+    
 }
 
 function makingWordlboxes(){
-    let word = "word";
     for (let i = 0; i < 4; i++){
         let wordle = document.createElement("div");
         wordle.id = i;
-        wordle.innerText = word[i];
-        console.log(wordle.innerText[0])
+        // wordle.innerText = word[i];
         wordle.classList.add("letter");
         document.getElementById("theWord").appendChild(wordle);
     }
 
-    for (let i = 0; i < 4; i++){
-        let wordle = document.createElement("div");
-        wordle.id = i;
-        wordle.innerText = word[i];
-        console.log(wordle.innerText[0])
-        wordle.classList.add("letter");
-        document.getElementById("theWord").appendChild(wordle);
+    for (let j = 0; j < 4; j++){
+        let worl = document.createElement("div");
+        worl.id = j;
+        worl.addEventListener("click", select);
+        worl.classList.add("letter");
+        document.getElementById("theWord").append(worl);
+    }
+}
+
+
+function select() {
+    if (lSel && lSel.id != (keys.length-1)) {
+        this.innerText = keys[lSel.id]; 
+    }
+}
+
+function letterSel(){
+    if (lSel) {
+        lSel.classList.remove("letterSelect");
+    }
+    lSel = this;
+    if(lSel.id == (keys.length-1)){
+
+    }else if(lSel.id != (keys.length-1)){
+        lSel.classList.add("letterSelect");
+        console.log("wordle.id");
     }
 }
