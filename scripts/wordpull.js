ce66ac39a7cdfae3c751482aa305e5c0cd0d0f13
Zachariah Friesen <zachariah.friesen@ontariotechu.net>
2023-04-19 16:38:38 -0400

    api works now thank god

--- scripts/wordpull.js
+++ scripts/wordpull.js
@@ -1,7 +1,22 @@
+// var unirest = require('unirest');
 function dictionarypull(a){
-    unirest.get("https://wordsapiv1.p.mashape.com/words/"+a+"/definitions").header("X-Mashape-Key", "f4e1640476mshdb930a1bc0b6df0p141dc5jsn5751248fbbca").end(function(b){
-        var temp = JSON.stringify(b);
-        var data = JSON.parse(temp);
-        document.getElementById('dict').innerHTML = "<br> <p>Definitions: "+data.Search[0].definition+"</p> <br>";
+    // const options = {
+    //     method: 'GET',
+    //     headers: {
+    //         'X-RapidAPI-Key': 'f4e1640476mshdb930a1bc0b6df0p141dc5jsn5751248fbbca',
+    //         'X-RapidAPI-Host': 'wordsapiv1.p.rapidapi.com'
+    //     }
+    // };
+    
+    // fetch('https://wordsapiv1.p.rapidapi.com/words/.22-caliber/pertainsTo', options)
+    //     .then(response => response.json())
+    //     .then(response => console.log(response))
+    //     .catch(err => console.error(err));
+    $.get("https://api.dictionaryapi.dev/api/v2/entries/en/"+a, function(b){
+        var mean = b[0].meanings;
+        var defs = mean[0].definitions;
+        var def = defs.definition;
+        var ex = defs.example;
+        document.getElementById('dict').innerHTML = "<br> <p>Definition: "+defs.definition+"</p> <br> <br> <p>Example use: "+defs.example+"</p> <br>";
     });
 }
