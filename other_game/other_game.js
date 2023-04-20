0dbb5211a674ac93f6a780b7498dfd61c77c1ea6
Dimitrios Kesikiadis <dimitrios.kesikiadis@ontariotechu.net>
2023-04-20 02:15:45 -0400

    fixed navbar issue with other_game

--- other_game/other_game.js
+++ other_game/other_game.js
@@ -3,12 +3,14 @@
 var myCharacter;
 
 function startGame() {
+    // creating elements
     gameArea.start()
     myCharacter = new character(); 
     myTarget = new target();
     myScore = new score();
 }
 
+// game board
 var gameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
@@ -16,9 +18,9 @@ var gameArea = {
         this.canvas.height = 400;
         this.context = this.canvas.getContext("2d")
         this.canvas.style.cursor = "none"
-        document.body.insertBefore(this.canvas, document.body.childNodes[0])
+        document.body.insertBefore(this.canvas, document.body.childNodes[2])
         // sets the interval of frame rate 
-        this.frames = setInterval(updateScreen, 15)
+        this.frames = setInterval(updateScreen, 10)
 
         // computing mouse movevements
         window.addEventListener('mousemove', function(event) {
@@ -40,6 +42,7 @@ var gameArea = {
 
 }
 
+// creates square that follows cursor
 function character() {
     this.x = 20;
     this.y = 380;
@@ -62,18 +65,22 @@ function target() {
     this.width = 80;
     this.height = 80;
 
+    // draws the character in the update fucntion so that it can be called many times a second
     this.update = function() {
         contx = gameArea.context;
         contx.fillStyle = "red";
         contx.fillRect(this.x, this.y, this.width, this.height);
     }
 
+    // function gets location of mouse and detects the hovering
     this.hover = function() {
         var left = this.x;
         var right = this.x + (this.width);
         var top = this.y;
         var bottom = this.y + (this.height);
         var hovered = true;
+
+        // determines of the square has touched the target
         if ((bottom < gameArea.y) || (top > gameArea.y) || (right < gameArea.x) || (left > gameArea.x)) {
             hovered = false;
         }
@@ -82,6 +89,7 @@ function target() {
 
 }
 
+// score text
 function score() {
     this.x = 600;
     this.y = 30;
@@ -95,26 +103,35 @@ function score() {
     }
 }
 
+// creates the elements on screen 
 function updateScreen() {
 
+    // variables for random target movement
     var xRandPosition = Math.floor(Math.random() * (650 - 0 + 1)) + 1; 
     var yRandPosition = Math.floor(Math.random() * (350 - 0 + 1)) + 1;
+
+    // variables for hits 
     var hits = 0;
 
+    // clears everything 
     gameArea.clear();
 
+    // score text
     myScore.text = "HITS: "
 
     if (gameArea.x && gameArea.y) {
 
+        // makes the square follow the mouse
         myCharacter.x = gameArea.x;
         myCharacter.y = gameArea.y;
 
         if(myTarget.hover()) {
 
+            // randomizes position of target after hovered over 
             myTarget.x = xRandPosition;
             myTarget.y = yRandPosition;
             
+            // increments hit counter
             hits += 1
             myScore.text = "HITS: " + hits
 
@@ -122,6 +139,7 @@ function updateScreen() {
         
     }
     
+    // updates the screen by drawing the elements 
     myTarget.update();
     myScore.update();
     myCharacter.update();
