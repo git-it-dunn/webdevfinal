f64cfe18cf9dd4902b55b3a295411662ede3704b
Zachariah Friesen <zachariah.friesen@ontariotechu.net>
2023-04-20 02:04:08 -0400

    commented dictpull

--- scripts/wordpull.js
+++ scripts/wordpull.js
@@ -1,22 +1,9 @@
-// var unirest = require('unirest');
 function dictionarypull(a){
-    // const options = {
-    //     method: 'GET',
-    //     headers: {
-    //         'X-RapidAPI-Key': 'f4e1640476mshdb930a1bc0b6df0p141dc5jsn5751248fbbca',
-    //         'X-RapidAPI-Host': 'wordsapiv1.p.rapidapi.com'
-    //     }
-    // };
-    
-    // fetch('https://wordsapiv1.p.rapidapi.com/words/.22-caliber/pertainsTo', options)
-    //     .then(response => response.json())
-    //     .then(response => console.log(response))
-    //     .catch(err => console.error(err));
-    $.get("https://api.dictionaryapi.dev/api/v2/entries/en/"+a, function(b){
-        var mean = b[0].meanings;
-        var defs = mean[0].definitions;
+    $.get("https://api.dictionaryapi.dev/api/v2/entries/en/"+a, function(b){ //pulls json file from link, a variable is word function gets desc of
+        var mean = b[0].meanings;       
+        var defs = mean[0].definitions;        //this and last line are path to definition and example in json file
         var def = defs[0].definition;
-        var ex = defs[0].example;
-        document.getElementById('dict').innerHTML = "<br> <p>Definition: "+JSON.stringify(def)+"</p> <br>";
+        var ex = defs[0].example;       
+        document.getElementById('dict').innerHTML = "<br> <p>Definition: "+JSON.stringify(def)+"</p> <br>";     //prints out into object with 'dict' id. also stringifys definition
     });
 }
